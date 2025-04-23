659e16c3efceba0a83724730e9073488ce9854dd
Egor <egorguk2005@outlook.com>
2025-04-23 14:12:10 +0500

    - улучшена структура EnvironmentApplication.java

--- src/main/java/ru/guk/education/EnvironmentApplication.java
+++ src/main/java/ru/guk/education/EnvironmentApplication.java
@@ -1,5 +1,7 @@
 package ru.guk.education;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,22 +11,14 @@ import ru.guk.education.injection.SetterInjectionService;
 import ru.guk.education.service.NotificationService;
 
 @SpringBootApplication
+@Log4j2
+@RequiredArgsConstructor
 public class EnvironmentApplication  implements CommandLineRunner {
 
     private final ConstructorInjectionService constructorService;
     private final FieldInjectionService fieldService;
     private final SetterInjectionService setterService;
     public final NotificationService notificationService;
-
-    public EnvironmentApplication(NotificationService notificationService,
-                                  SetterInjectionService setterService,
-                                  ConstructorInjectionService constructorService,
-                                  FieldInjectionService fieldService) {
-        this.notificationService = notificationService;
-        this.setterService = setterService;
-        this.constructorService = constructorService;
-        this.fieldService = fieldService;
-    }
 
 
     public static void main(String[] args) {
@@ -33,7 +27,16 @@ public class EnvironmentApplication  implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        log.info("Starting application logic...🎇");
+        notifyAllServices();
+        demonstrateInjections();
+    }
+
+    private void notifyAllServices() {
         notificationService.notifyAllServices("Какое-то сообщение");
+    }
+
+    private void demonstrateInjections() {
         constructorService.process();
         fieldService.process();
         setterService.process();
